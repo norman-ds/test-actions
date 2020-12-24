3ef2511a5f701504a9e4bc25dfbe062f6ee6819d
Norman <normandev@puntaminar.ch>
2020-12-24 12:35:50 +0100

    initial

--- opendata/mining/opendata.R
+++ opendata/mining/opendata.R
@@ -1,1 +1,28 @@
-Hallo Velo
+# Weekly death data in Switzerland
+# Download from opendata.swiss
+
+# library(curl::curl_download)
+# library(jsonlite::fromJSON)
+
+#"https://opendata.swiss/de/dataset?q=%22Todesfälle+nach+Fünf-Jahres-Altersgruppe%22+Kanton"
+
+# list of all apis
+openlist <- jsonlite::fromJSON('https://opendata.swiss/api/3/action/package_list')
+stopifnot(openlist$success)
+
+# give list of all ids
+pivot.slug <- "todesf.*lle.*alter.*kanton.*csv"
+idlist <- openlist$result[grepl(pivot.slug, openlist$result)]
+
+# download datasets (metadata and urls) by id (slug)
+pdown <- function(id) {
+  myurl <- "https://opendata.swiss/api/3/action/package_show"
+  
+  if (!dir.exists('data')) dir.create('data')
+  if (!dir.exists(file.path('data',Sys.Date()))) dir.create(file.path('data',Sys.Date()))
+  myfile <- file.path('data',Sys.Date(), id)
+  
+  curl::curl_download(paste0(myurl,'?id=', id), myfile)
+}
+lapply(idlist, pdown)
+
---
+++ opendata/mining/test_opendata.R
@@ -0,0 +1,77 @@
+## testing functions opendata
+
+library(dplyr)
+# library(jsonlite::fromJSON)
+
+myid <- 'todesfalle-nach-funf-jahres-altersgruppe-geschlecht-woche-und-kanton-csv-datei33'
+myfile <- file.path('data', '2020-12-24', myid)
+
+# shows one dataset by id (slug)
+pshow <- function(id) {
+  res <- jsonlite::fromJSON(id)
+  stopifnot(res$success)
+  return(res)
+}
+
+# give all meta information of one api
+pshow(myfile) 
+
+# shows selected metadata in a table
+ptable <- function(id) {
+  rest_api <- pshow(id)
+  stopifnot(rest_api$success) 
+  
+  as.na <- function(cell) {
+    ifelse(is.null(cell), NA, cell)
+  }
+  
+  r <- rest_api$result
+  api_desc <- tibble(id=id,
+                     issued=r$issued,
+                     author=r$author,
+                     language=r$language,
+                     spatial=as.na(r$spatial),
+                     metadata_created=r$metadata_created,
+                     metadata_modified=r$metadata_modified,
+                     start_date=as.na(r$temporals$start_date),
+                     end_date=as.na(r$temporals$end_date),
+                     display_name=r$display_name$de,
+                     state=r$state,
+                     format=paste(sort(unique(r$resources$format)),collapse = ','))
+  
+  return(api_desc)
+}
+
+# give selected metadata of apis with pivot.slug
+ptable(myfile) %>%
+  filter(language=='de') %>% t
+
+# shows selected api data in a table
+papi <- function(id, formats=c('CSV'), language='de') {
+  rest_api <- pshow(id)
+  stopifnot(rest_api$success) 
+  
+  as.na <- function(cell) {
+    ifelse(is.null(cell), NA, cell)
+  }
+  
+  r <- filter(rest_api$result$resources, format %in% formats)
+  #return(r)
+  api_desc <- tibble(id=id,
+                     issued=r$issued,
+                     display_name=unlist(r$display_name[language]),
+                     language=unlist(r$language),
+                     created=r$created,
+                     format=r$format,
+                     rights=r$rights,
+                     state=r$state,
+                     url=r$url,
+                     download_url=r$download_url,
+                     revision_id=r$revision_id)
+  
+  return(api_desc)
+}
+
+# give selected download urls of apis with pivot.slug
+y <- papi(myfile)
+unlist(y[1,])
