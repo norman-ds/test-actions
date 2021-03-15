222d9db57505e69b7998ca6074ca11ed1232691d
Norman Bieri <60508250+norman-ds@users.noreply.github.com>
2021-03-15 08:55:38 +0100

    Update fileread.R

--- projekt21/R/fileread.R
+++ projekt21/R/fileread.R
@@ -5,10 +5,11 @@ fileread <- function(datafile) {
   df <- readr::read_delim(datafile,
                           delim = ';',
                           col_types = cols(.default = col_character())) %>%
+    rename_all(toupper)%>%
     mutate(YEAR=gsub('^(.{4}).*','\\1',TIME_PERIOD), 
            CW=gsub('.*(.{2})$','\\1',TIME_PERIOD))
   
-  df$VALUE=parse_integer(df$Obs_value)
+  df$VALUE=parse_integer(df$OBS_VALUE)
   stop_for_problems(df$VALUE)
   
   # reference column names
@@ -16,8 +17,8 @@ fileread <- function(datafile) {
                 'GEO',
                 'AGE',
                 'SEX',
-                'Obs_status',
-                'Obs_value',
+                'OBS_STATUS',
+                'OBS_VALUE',
                 'YEAR',
                 'CW',
                 'VALUE')
